74ddeb0e16fcb22643617bfe65ff20cf0a3bb5d4
santostimoteo21@gmail.com <santostimoteo21@gmail.com>
2023-11-08 06:42:33 -0300

    iniciando os estudos de javascript

---
+++ 01 - Variaveis/VARIAVEIS E CONSTANTES.js
@@ -0,0 +1,28 @@
+/*
+1. É UMA LINGUAGEM CASE SENSITIVE
+
+*/
+
+//DECLARACAO DE VARIAVEIS
+var nome;
+nome = "timoteo";
+var idade = 10.00;
+
+//declaracao de constante
+const CIDADE = "SAO LOURENCO";
+
+//comando de saida
+console.log(" CIDADE = " + CIDADE);
+console.log(" NOME " + nome);
+console.log(" IDADE " + idade);
+console.log("--------------------");
+
+//CRIANDO FUNCOES
+function abrirJanela()
+{
+    document.getElementById("ret2").innerHTML = 2 + 2;
+}
+
+
+
+
---
+++ 02 - Acessando elementos/acessando elementos.js
@@ -0,0 +1,17 @@
+      <!-- podemos manipular a informacao do html com javascrip
+      como veremos a seguir o javascrip mofificando o texto de um 
+      paragrafo
+
+      PARAGRAGO QUE TERA O TEXTO MODIFICADO -->
+      <p id="demo"> Texto de um cabeçalho. </p>
+      <!-- botao que vai invocar a funcao que modigica o texto -->
+      <button type="button" onclick="minhaFuncao()"> Try it </button>
+    
+    <script>
+      //funcao que troca o texto de um elemento que tenha como id = demo      
+      function minhaFuncao(){
+       //buscando o elemento atraves do getElementById("identificador")
+       //o innerHTML = "enviarei para o elemento"
+      document.getElementById("demo").innerHTML = "texto";
+      }
+    </script>
